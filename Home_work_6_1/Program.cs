886f4182dd38d72dc9ca53a74b977ed094ff3a3f
Jasur <jasurbek19971997@mail.ru>
2023-07-26 07:11:00 +0500

    Home work 6

---
+++ Home_work_6_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+Console.Clear();
+int Prompt(string message){
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+void InputArray(int[] arr){
+    for(int i=0;i<arr.Length;i++){
+        arr[i]=new Random().Next(-10,10);
+    }
+}
+int ReleaseArray(int[] arr){
+    int count = 0;
+    for(int i=0;i<arr.Length;i++){
+        if(arr[i]>0){
+            count++;
+        }
+    }
+    return count;
+}
+int number = Prompt("Введите кол-во элементов в массиве: ");
+Console.WriteLine();
+
+int[] array=new int[number];
+InputArray(array);
+Console.Write($"[ {string.Join(", ", array)} ]");
+Console.WriteLine();
+
+// ReleaseArray(array);
+Console.WriteLine($"Положительные элементов: N = {ReleaseArray(array)}");
+
---
+++ Home_work_6_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения 
+// b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+double Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+void FindPoints(double k1, double b1, double k2, double b2)
+{
+    if (k2 == k1)
+    {
+        Console.WriteLine("Функции не пересекается!");
+    }
+    else
+    {
+        double x = Math.Round((b1 - b2) / (k2 - k1), 2);
+        double y = Math.Round(k1 * ((b1 - b2) / (k2 - k1)) + b1, 2);
+        Console.WriteLine($"Ответ: x = {x}; y = {y}");
+    }
+}
+
+double k1 = Prompt("Введете число: k1 = ");
+double b1 = Prompt("Введете число: b1 = ");
+
+double k2 = Prompt("Введете число: k2 = ");
+double b2 = Prompt("Введете число: b2 = ");
+FindPoints(k1, b1, k2, b2);
+
+// Console.Write("Vvedete chislo: k1 = ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Vvedete chislo: b1 = ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Vvedete chislo: k1 = ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Vvedete chislo: b1 = ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = (b1-b2)/(k2-k1);
+// double y = k1*((b1-b2)/(k2-k1))+b1;
+// Console.WriteLine($"x = {x}; y = {y}");
